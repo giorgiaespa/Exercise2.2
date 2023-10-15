1b976cceb3df1c6520ed0d6be864b56f50a04a44
gio <giorgiaespa2.0@gmail.com>
2023-10-15 21:55:53 +0200

    Initial commit

---
+++ Car.java
@@ -0,0 +1,51 @@
+public class Car {
+    public String brand;
+    public String model;
+    public String plate;
+    public int displacement;
+
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public int getDisplacement() {
+        return displacement;
+    }
+    public void setDisplacement(int displacement) {
+        this.displacement = displacement;
+    }
+
+    public Car(String brand, String model, String plate, int displacement) {
+        this.brand = brand;
+        this.model = model;
+        this.plate = plate;
+        this.displacement = displacement;
+    }
+
+    @Override
+    public String toString() {
+        return "Car = " +
+                "brand:'" + brand + '\'' +
+                ", model:'" + model + '\'' +
+                ", plate:'" + plate + '\'' +
+                ", displacement:" + displacement;
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Range Rover", "Velar", "DZ435CW", 2000);
+
+        System.out.println(car);
+    }
+}
